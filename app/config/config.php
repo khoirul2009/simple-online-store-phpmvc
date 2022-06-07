164b6184e209b7088f356a9ebc4005d94f6b3f2d
Muhammad Khoirul Afwan <86579261+khoirul2009@users.noreply.github.com>
2022-06-08 06:18:26 +0700

    Update config.php

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define("BASEURL", "http://localhost/AppMVC/public/");
 
 // DB
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'phpmvc');
+define('DB_HOST', 'ec2-3-211-221-185.compute-1.amazonaws.com');
+define('DB_USER', 'omotkxxxxgwbgz');
+define('DB_PASS', '4114a4c66a1b91f931fa72bc52eed170e2457ef7ebec8370d03c8f4386bd0639');
+define('DB_NAME', 'db3tkl3deq4h3o');
